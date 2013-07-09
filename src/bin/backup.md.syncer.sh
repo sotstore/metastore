c5cb283061092624950dfd063b8d4e8f9c3318d5
Ma Can <ml.macana@gmail.com>
2013-07-09 12:30:38 +0800

    Update backup.md.syncer.sh, more verbose.

--- src/bin/backup.md.syncer.sh
+++ src/bin/backup.md.syncer.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
 if [ x"$RHOME" == "x" ]; then
-	echo "detect RHOME now ..."
-	RHOME=`pwd`/backup
+    echo "detect RHOME now ..."
+    RHOME=`pwd`/backup
 else
-	echo "Use RHOME=$RHOME"
+    echo "Use RHOME=$RHOME"
 fi
 
 
 if [ x"$RUSER" == "x" ]; then
-	echo "Please set RUSER={remote user name}"
-	exit
+    echo "Please set RUSER={remote user name}"
+    exit
 fi
 
 if [ x"$RNODE" == "x" ]; then
-	echo "Please set RNODE={remote backup node}"
-	exit
+    echo "Please set RNODE={remote backup node}"
+    exit
 fi
 
 if [ x"$RPATH" == "x" ]; then
-	echo "Please set RPATH={remote backup path}"
-	exit
+    echo "Please set RPATH={remote backup path}"
+    exit
 fi
 
 echo -n "Try to use $RHOME as SYNC Metadata Home."
 if [ -d $RHOME ]; then
-	echo " ... OK."
+    echo " ... OK."
 else
-	echo " ... BAD. Directory not exist!"
-	exit
+    echo " ... BAD. Directory not exist!"
+    exit
 fi
 
 cd $RHOME
+echo "Checking MD @ `date` ..."
 for d in `ls $RHOME`; do
-	echo -n "Scan Directory '$d' ..."
-	if [ -s $d/manifest.desc ]; then
-		echo " OK."
-		# sync it 
-		#scp -r "$RHOME/${d//:/\:}" $RUSER@$RNODE:$RPATH/
-		scp -r "$RHOME/${d}" $RUSER@$RNODE:$RPATH/
-	else
-		echo " ZERO, do not sync it."
-		# delete it
-		#rm -rf $d
-	fi
+    echo -n "Scan Directory '$d' ..."
+    if [ -s $d/manifest.desc ]; then
+        echo " OK."
+        # sync it 
+        #scp -r "$RHOME/${d//:/\:}" $RUSER@$RNODE:$RPATH/
+        echo " -->" scp -r "$RHOME/${d}" $RUSER@$RNODE:$RPATH/
+        scp -r "$RHOME/${d}" $RUSER@$RNODE:$RPATH/
+    else
+        echo " ZERO, do not sync it."
+        # delete it
+        #rm -rf $d
+    fi
 done
