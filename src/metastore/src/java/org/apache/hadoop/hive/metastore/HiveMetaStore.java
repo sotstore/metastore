4b959f949e39d7ed1105ca467a5c3038fb9c2b69
caoruyi <caoruyi@vip.qq.com>
2013-09-22 21:33:02 +0800

    rpc of equiproom and geolocation and nodeassignment

--- src/metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ src/metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -5785,7 +5785,7 @@ public class HiveMetaStore extends ThriftHiveMetastore {
     public boolean addEquipRoom(EquipRoom er) throws MetaException, TException {
       LOG.info(er);
       getMS().addEquipRoom(er);
-      return false;
+      return true;
     }
 
     @Override
