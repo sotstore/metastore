9f6bbed1b4603cc489c1336a9033a0d63d8af5a8
caoruyi <caoruyi@vip.qq.com>
2013-09-24 18:47:41 +0800

    modify the geolocation

--- src/metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
+++ src/metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
@@ -86,6 +86,7 @@ import org.apache.hadoop.hive.metastore.api.UnknownPartitionException;
 import org.apache.hadoop.hive.metastore.api.UnknownTableException;
 import org.apache.hadoop.hive.metastore.api.User;
 import org.apache.hadoop.hive.metastore.model.MetaStoreConst;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.shims.HadoopShims;
 import org.apache.hadoop.hive.shims.ShimLoader;
 import org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge;
